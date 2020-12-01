23a9a3102e4d4c1ee048cff654809c93707a0c2b
Your NameNikolay <nikolya_usov@bk.ru>
2020-12-01 13:19:08 +0300

    Исправил замечания наставника

--- source/js/burger.js
+++ source/js/burger.js
@@ -2,7 +2,7 @@ const mainNavToggle= document.querySelector(".main-nav__toggle");
 const navMain = document.querySelector(".main-nav");
 navMain.classList.remove('main-nav--nojs');
 
-mainNavToggle.addEventListener("click",function(evt){
+mainNavToggle.addEventListener("click" ,function(evt){
   evt.preventDefault();
   navMain.classList.toggle("main-nav--open");
 });
