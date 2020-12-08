24ce97abbddde95ea97cb0944d631d2c8bdc5f9a
Nikolay <nikolya_usov@bk.ru>
2020-12-08 20:19:56 +0300

    Начал оптимизацию

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,54 @@ const sass = require("gulp-sass");
 const postcss = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
 const sync = require("browser-sync").create();
+const  csso = require("postcss-csso");
+const rename = require("gulp-rename");
+const htmlmin = require("gulp-htmlmin");
+const uglify = require("gulp-uglify-es");
+const imagemin = require("gulp-imagemin");
+const webp = require("gulp-webp");
+const svgstore  = require("gulp-svgstore");
+const del = require("del");
+//
 
+const images = () => {
+  return gulp.src("source/img/**/*.{jpg,png,svg}")
+  .pipe(imagemin([
+    imagemin.mozjpeg({progressive: true}),
+    //imagemin.optipng({optimizationLevel: 3}),
+    imagemin.svgo()
+  ]))
+  .pipe(gulp.dest("build/img"))
+}
+
+exports.images = images;
+
+//webP
+
+const createWebp = () => {
+  return gulp.src("source/img/**/*.{jpg,png}")
+  .pipe(webp({quality: 90}))
+  .pipe(gulp.dest("build/img"))
+}
+exports.createWebp = createWebp;
+
+//sprite
+const sprite = () => {
+  return gulp.src("source/img/**/*.svg")
+  .pipe(svgstore())
+  .pipe(rename("sprite.svg"))
+  .pipe(gulp.dest("build/img"))
+}
+exports.sprite = sprite;
+// html
+
+const html = () => {
+  return gulp.src("source/**/*html")
+  .pipe(htmlmin({collapseWhitespace: true}))
+  .pipe(gulp.src("build"))
+}
+
+exports.html = html;
 // Styles
 
 const styles = () => {
@@ -14,15 +61,44 @@ const styles = () => {
     .pipe(sourcemap.init())
     .pipe(sass())
     .pipe(postcss([
-      autoprefixer()
+      autoprefixer(),
+      csso()
     ]))
     .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("source/css"))
+    .pipe(rename("styles.min.css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(sync.stream());
 }
 
 exports.styles = styles;
 
+// scripts
+const scripts = () => {
+  return gulp.src("source/js/script.js")
+  //.pipe(uglify())
+  .pipe(rename("scripts.min.js"))
+  .pipe(gulp.dest("build/js"))
+  .pipe(sync.stream());
+}
+exports.scripts = scripts;
+// copy
+const copy = () => {
+  return gulp.src([
+    "source/fonts/*.{.woff2,woff}",
+    "source/img/**/*.{jpg,png,svg}"
+  ],
+  {
+    base: "source"
+  })
+  .pipe(gulp.dest(build))
+}
+exports.copy = copy;
+
+//del
+const clean = () => {
+  return del("build");
+}
+exports.clean = clean;
 // Server
 
 const server = (done) => {
@@ -39,6 +115,18 @@ const server = (done) => {
 
 exports.server = server;
 
+//build
+const build = gulp.series(
+  clean,
+  gulp.parallel (
+    styles,
+    html,
+    sprite,
+    images,
+    createWebp
+  )
+)
+exports.build = build;
 // Watcher
 
 const watcher = () => {
---
+++ source/js/script.js
@@ -0,0 +1,8 @@
+const mainNavToggle= document.querySelector(".main-nav__toggle");
+const navMain = document.querySelector(".main-nav");
+navMain.classList.remove('main-nav--nojs');
+
+mainNavToggle.addEventListener("click" ,function(evt){
+  evt.preventDefault();
+  navMain.classList.toggle("main-nav--open");
+});
